f0e33cd70b67862bf687478ed575fe1dbb2e6a4b
Wesly Clark <NWLI1620@gmail.com>
2013-03-25 10:42:07 -0500

    GO DEEPER!!

---
+++ G.R.A.D.E/G.R.A.D.E/GRADEMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  GRADEMasterViewController.h
+//  G.R.A.D.E
+//
+//  Created by Nathan Wesly on 3/3/13.
+//  Copyright (c) 2013 GRADETEAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GRADEDetailViewController;
+
+@interface GRADEMasterViewController : UITableViewController
+
+@property (strong, nonatomic) GRADEDetailViewController *detailViewController;
+
+@end
